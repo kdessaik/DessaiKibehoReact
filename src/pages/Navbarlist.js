31bc567b7ba214952307f103d3942ca8015e9a0a
Kambale Kibeho <dessaikibeho@gmail.com>
2022-05-11 14:27:57 +0300

    kdk

--- src/pages/Navbarlist.js
+++ src/pages/Navbarlist.js
@@ -6,8 +6,8 @@ const Navbar = () => {
     <div className="navbar">
       <h1 id="Myname1"><img src={require('../Image/df.jpg')}/>Dessai</h1>
       <Link id='pages' to="/">Home</Link>
-      <Link  id='pages' to="/about">About</Link>
-      <Link  id='pages' to="/My-Work">My-Work</Link>
+      <Link  id='pages2' to="/about">About</Link>
+      <Link  id='pages3' to="/My-Work">My-Work</Link>
       <div id='email'>
         <img src={require('../Image/message1.jpg')}/>
       <a id='email1' href='Kdessaikibeho@gmail.com'>Kdessaikibeho@gmail.com</a>
